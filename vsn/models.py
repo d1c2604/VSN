289d37c0be67bb0082398bc71e46172ebeee6100
D1Ce-1 <96570839+d1c2604@users.noreply.github.com>
2022-07-01 07:23:10 +0530

    1u

--- vsn/models.py
+++ vsn/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-#code for users
+class User(models.Model):
+    uname = models.CharField(max_length=20,blank=False)
+    uid = models.CharField(max_length=10, blank = False)
+    phone = models.IntegerField(max_length=10)
+    mail = models.EmailField(blank=True)
+    pawd = models.TextField(blank=False)
+
+    
--- vsn/views.py
+++ vsn/views.py
@@ -3,7 +3,20 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request,'index.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        uid = request.POST['uid']
+        mail = request.POST['mail']
+        phone = request.POST['phone']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        log_stat = False
+        
+
+        return render(request,'index.html',{})
+    
+    else:
+        return render(request,'index.html')
 
 
 def contact(request):
